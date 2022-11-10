fd67cc1de659fb8f6f2bd122f7472bf56f09efca
Tyler Seiford <50031864+TylerSeiford@users.noreply.github.com>
2022-11-09 21:01:51 -0600

    Create script.sh

---
+++ script.sh
@@ -0,0 +1,10 @@
+for id in {00000..00029};
+do
+    convert ../apriltag-imgs/tag16h5/tag16_05_$id.png \
+        -density 300 \
+        -scale $((100 * 300 / 10 * 8))% \
+        -bordercolor black -border 1 \
+        -gravity center -extent $((300*17/2))x$((300*11)) \
+        -gravity south -annotate +0+$((300/4)) "AprilTag family = tag16h5, size = 8 in, id = $id" \
+        tag15h6/us_letter/8in/tag16h5_8in_id$id.pdf
+done
